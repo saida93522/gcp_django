299b125763f078903281b579c96582c84507f254
saida93522 <54591143+saida93522@users.noreply.github.com>
2021-11-17 17:06:07 -0600

    file config

--- hello_app/views.py
+++ hello_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def hello(request):
-    return HttpResponse('Hello GCP!')
+    return HttpResponse('Hello! This Django app deployed through GCP!!!')
